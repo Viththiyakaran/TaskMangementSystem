f0b85fff41717d87e9062f6c41645ebcda134889
Viththiyakaran <battikaran6@gmail.com>
2023-07-22 20:43:08 +0530

    Login-API-Logout-Fix

--- frontend/src/app/service/token-pass.interceptor.ts
+++ frontend/src/app/service/token-pass.interceptor.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('userToken'); // Get the JWT token from localStorage
-    console.log("InterCEpt",token);
+
     if (token) {
       const cloned = request.clone({
         setHeaders: {
@@ -14,7 +18,15 @@ export class AuthInterceptor implements HttpInterceptor {
         }
       });
 
-      return next.handle(cloned);
+      return next.handle(cloned).pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 401) {
+            // Handle token expiration: Redirect to the login page
+            this.router.navigate(['/login']);
+          }
+          return throwError(error);
+        })
+      );
     }
 
     return next.handle(request);
